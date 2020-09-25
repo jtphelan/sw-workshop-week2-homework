83f151354a92cb94f4002f425cbc8384dfa23bb4
NataliaTepluhina <tarya.se@gmail.com>
2020-09-25 16:09:41 +0300

    feat: installed Apollo

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import VueRouter from 'vue-router';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
 import routes from './routes';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
 
 Vue.use(VueRouter);
 
@@ -12,7 +18,27 @@ const router = new VueRouter({ routes });
 
 Vue.config.productionTip = false;
 
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://rickandmortyapi.com/graphql/',
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
 new Vue({
   render: (h) => h(App),
   router,
+  apolloProvider,
 }).$mount('#app');

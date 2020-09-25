be7c4da161d1244720436b46a48ade09ff314b1a
NataliaTepluhina <tarya.se@gmail.com>
2020-09-25 10:59:14 +0300

    feat: added router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import '@babel/polyfill';
 import 'mutationobserver-shim';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
+import routes from './routes';
+
+const router = new VueRouter({ routes });
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount('#app');
